0b0afeb3d7eabae794f5a319a397738aec193762
Hans Dieter Pearcey <hdp@weftsoar.net>
2009-02-03 01:14:35 -0500

    start on docs

--- lib/MooseX/Constructor/AllErrors.pm
+++ lib/MooseX/Constructor/AllErrors.pm
@@ -29,3 +29,47 @@ sub init_meta {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+MooseX::Constructor::AllErrors - capture all constructor errors
+
+=head1 SYNOPSIS
+
+  package MyClass;
+  use MooseX::Constructor::AllErrors;
+
+  has foo => (is => 'ro', required => 1);
+  has bar => (is => 'ro', isa => 'Int');
+
+  ...
+
+  eval { MyClass->new };
+  # $@->errors has two errors, not just the missing required attribute
+
+=head1 DESCRIPTION
+
+MooseX::Constructor::AllErrors tries to capture every error generated during
+the construction of your objects, rather than halting after the first.
+
+If there are errors, C<$@> will contain a
+L<MooseX::Constructor::AllErrors::Error::Constructor> object.  See its
+documentation for possible error types.
+
+=head1 SEE ALSO
+
+L<Moose>
+
+=head1 AUTHOR
+
+  Hans Dieter Pearcey <hdp@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Hans Dieter Pearcey. This is free
+software; you can redistribute it and/or modify it under the same terms as perl
+itself. 
+
+=cut
